dccd20bb0d824b7774ddc6f46c6626f78ce0a18f
han1202012 <octopus_truth@163.com>
2021-03-28 21:46:09 +0800

    update

--- lib/main3.dart
+++ lib/main3.dart
@@ -4,51 +4,36 @@ void main() {
   runApp(AnimationApp());
 }
 
-/// 3 . 定义纯组件, 动画应用与该组件上
-class AnimationWidget extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return // 动画的主体组件
-      // 布局组件中使用动画的值 , 以达到动画效果
-      Container(
-        decoration: BoxDecoration(color: Colors.red),
-      );
-  }
-}
+/// 1. 定义动画组件, 动画的组件封装在该组件中
+/// 使用 AnimatedWidget 快速实现一个动画
+class AnimatedApp extends AnimatedWidget{
 
-/// 4 . 将组件与动画结合起来
-class AnimationTransition extends StatelessWidget{
-  /// 构造方法
-  AnimationTransition({this.child, this.animation});
-
-  /// 动画作用的组件
-  final Widget child;
-  /// 动画
-  final Animation<double> animation;
+  /// 构造函数
+  AnimatedApp({Key key, Animation<double> animation})
+      :super(key: key, listenable: animation);
 
   @override
   Widget build(BuildContext context) {
-    /// AnimatedBuilder 会自动监听 animation 的变化
-    /// 然后渲染 child 组件上的动画
+    /// 获取动画
+    Animation<double> animation = listenable;
     return Column(
       children: [
         Text("动画状态 : ${animation.status}", textDirection: TextDirection.ltr,),
 
-        Text("动画值 : ${animation.value?.round()}", textDirection: TextDirection.ltr,),
+        Text("动画值 : ${animation.value.round()}", textDirection: TextDirection.ltr,),
 
-        Container(height: 50,),
-
-        AnimatedBuilder(
-          animation: animation,
-          builder: (context, child) => Container(
-            height: animation.value,
-            width: animation.value,
-            child: child,
-          ),
-          child: child,
-        )
+        // 动画的主体组件
+        // 布局组件中使用动画的值 , 以达到动画效果
+        Container(
+          /// 设置距离顶部 20 像素
+          margin: EdgeInsets.only(top: 50),
+          height: animation.value,
+          width: animation.value,
+          decoration: BoxDecoration(color: Colors.red),
+        ),
       ],
     );
+
   }
 }
 
@@ -63,6 +48,7 @@ class AnimationApp extends StatefulWidget{
 /// 每个 StatefulWidget 都需要一个配套的 State 类
 class _AnimationAppState extends State<AnimationApp>
     with SingleTickerProviderStateMixin{
+
   /// 动画类
   Animation<double> animation;
   /// 动画控制器
@@ -72,7 +58,7 @@ class _AnimationAppState extends State<AnimationApp>
   void initState() {
     super.initState();
 
-    /// 1. 初始化动画控制器
+    /// 2. 初始化动画控制器
     animationController = AnimationController(
       // 动画绘制到屏幕外部时, 减少消耗
       vsync: this,
@@ -80,30 +66,38 @@ class _AnimationAppState extends State<AnimationApp>
       duration: Duration(seconds: 3),
     );
 
-    /// 2 . 构造 Tween 补间动画 ,
+
+    /// 3 . 构造 Tween 补间动画 ,
     /// 设置动画控制器 AnimationController 给该补间动画
     /// 动画的值是正方形组件的宽高
     animation = Tween<double>(
       begin: 0,
       end: 300
     ).animate(animationController);
+
   }
 
   /// 该方法与 initState 对应
   @override
   void dispose() {
+
     /// 释放动画控制器
     animationController.dispose();
+
     super.dispose();
   }
 
+
   @override
   Widget build(BuildContext context) {
-    return
-      Column(
-        children: [
 
-          Container(height: 50,),
+    return Container(
+
+      /// 设置距离顶部 20 像素
+      margin: EdgeInsets.only(top: 100),
+
+      child: Column(
+        children: [
 
           GestureDetector(
             // 5 . 点击按钮开启动画
@@ -111,8 +105,10 @@ class _AnimationAppState extends State<AnimationApp>
               /// 按钮点击事件
               /// 首先将动画初始化
               animationController.reset();
+
               /// 正向执行动画, 即从初始值执行到结束值
               animationController.forward();
+
             },
             child: Container(
               alignment: Alignment.center,
@@ -127,15 +123,14 @@ class _AnimationAppState extends State<AnimationApp>
             ),
           ),
 
-          Container(height: 50,),
+          // 动画的主体组件
+          // 4 . 创建动画组件, 传入动画对象 animation
+          AnimatedApp(animation: animation,),
 
-          AnimationTransition(animation: animation, child: AnimationWidget())
 
         ],
-
-      );
-
-
+      ),
+    );
   }
 
 }
